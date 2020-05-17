5b2d7e665514795bb0b62375534b656a673566e3
franbustillo93 <Shawnofexpress21>
2020-05-18 01:14:00 +0200

    [Unity][Scene][Scripts] Added a simple scripting system to display the text information through a scriptable object. Also I've added the UI prefabs and reorganized the folder structure.

---
+++ Food_Wars_Demo/Assets/Scripts/Engine/ScriptableObjects/TextDialog.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TextDialog")]
+public class TextDialog : ScriptableObject
+{
+    [SerializeField] string characterName;
+    [TextArea(14, 10)] [SerializeField] string storyText;
+    
+
+    public string GetStoryText() {
+
+        return storyText;
+    }
+
+    public string GetStoryCharacterName() {
+        
+        return characterName;
+    }
+
+
+}
+
---
+++ Food_Wars_Demo/Assets/Scripts/UI/TextDialogs/TextManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager: MonoBehaviour
+{
+    [SerializeField] Text dialogText;
+    [SerializeField] Text characterName;
+    [SerializeField] TextDialog storyTextRef;
+    TextDialog storyText;
+
+    void Start()
+    {
+        
+        SetStoryTextRef();
+        SetStoryInformation();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetStoryTextRef(){
+        storyText = storyTextRef;
+    }
+
+    void SetStoryInformation() {
+
+        characterName.text = storyText.GetStoryCharacterName();
+        dialogText.text = storyText.GetStoryText();
+    }
+
+}
